ad3268a685dcd7f9491655e5d401917dba92f9b0
llorracc <git@llorracc.org>
2026-06-10 23:59:04 -0400

    Remove non-paper working dirs that rode in from the Latest tree
    (HARK/DemARK/matsya clones, session dirs, stray root files). The Latest->Public
    sync now excludes these going forward (HAFiscal-make 1fa9147).

--- HARK/HARK/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from HARK.ConsumptionSaving import *
-
-from HARK.Labeled.agents import PerfForesightLabeledType as PerfForesightLabeledType
-from HARK.Labeled.agents import IndShockLabeledType as IndShockLabeledType
-from HARK.Labeled.agents import RiskyAssetLabeledType as RiskyAssetLabeledType
-from HARK.Labeled.agents import PortfolioLabeledType as PortfolioLabeledType
